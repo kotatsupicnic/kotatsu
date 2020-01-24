cb55bb01e88a0057e0e0a79200c881431ba96369
kotapiku <ice08235523@gmail.com>
2020-01-24 14:55:48 +0900

    fix dokkaiahen

--- cron/dokkaiahen.py
+++ cron/dokkaiahen.py
@@ -13,10 +13,16 @@ from slackbot_settings import API_TOKEN
 
 def dokkaiahen():
     base_url = 'http://dka-hero.com/'
-    page = pq(url=base_url + 't_c.html', encoding='shift_jis')
-    link = page('a:first')
-    page2 = pq(url=base_url + 'mat/new.html', encoding='shift_jis')
-    link2 = page2('td:first')
+
+    page = pq(url=base_url + 't_m.html', encoding='shift_jis')  # 左の
+    horimiya_pq = page("center").eq(1)("table").eq(1)("tr").eq(1)("td").eq(1)("a")
+    horimiya = horimiya_pq.text()
+    aco_pq = page("center").eq(1)("table").eq(1)("tr").eq(4)("td").eq(1)("a")
+    aco = aco_pq.text()
+
+    page2 = pq(url=base_url + 'mat/new.html', encoding='shift_jis') # 更新履歴
+    others_pq = page2("td").eq(0)
+    others = others_pq.text()[:4]  # 日付
 
     data_url = urlparse.urlparse(os.environ['DATABASE_URL'])
     conn = psycopg2.connect(
@@ -28,38 +34,44 @@ def dokkaiahen():
     )
 
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM horimiya")    # horimiya
-    old = cursor.fetchone()[0]
-    cursor.execute("SELECT * FROM horimiya2")   # tanpen
-    old2 = cursor.fetchone()[0]
+    cursor.execute("SELECT * FROM horimiya")
+    horimiya_old = cursor.fetchone()[0]
+    cursor.execute("SELECT * FROM aco")
+    aco_old = cursor.fetchone()[0]
+    cursor.execute("SELECT * FROM others")
+    others_old = cursor.fetchone()[0]
 
-    newtext = link.text()
-    new = newtext[:4]   # 日付
-    newtextlist2 = link2.text().split()[0:3]
-    newtext2 = " ".join(newtextlist2)
-    new2 = newtext2[:4]
+    flag = True
+    new = []
+    if horimiya != horimiya_old:
+        cursor.execute("UPDATE horimiya SET title = " + repr(horimiya))
+        new.append({'title': horimiya, 'link': base_url + horimiya_pq.attr('href')})
+        flag = False
 
-    if new != old[:4]:
-        cursor.execute("UPDATE horimiya SET title = " + repr(new))
-        li = {'title': newtext, 'link': base_url + link.attr('href')}
+    if aco != aco_old:
+        cursor.execute("UPDATE aco SET title = " + repr(aco))
+        new.append({'title': aco, 'link': base_url + aco_pq.attr('href')})
+        flag = False
 
-    elif new2 != old2[:4]:
-        cursor.execute("UPDATE horimiya2 SET title = " + repr(new2))
-        li = {'title': newtext2, 'link': base_url}
+    if others != others_old:
+        cursor.execute("UPDATE others SET title = " + repr(others))
+        new.append({'title': others, 'link': base_url + "top.html"})
+        flag = False
 
-    else:
+    if flag:
         print('There is no update on {}'.format(base_url))
-        li = {}
 
     conn.commit()
     cursor.close()
     conn.close()
 
-    return li
+    return new
 
 if __name__ == '__main__':
     slack = Slacker(API_TOKEN)
-    res = dokkaiahen()
-    if res:
-        message = '読解アヘンに更新があります\n{title}\n{link}'.format(**res)
+    new = dokkaiahen()
+    if new:
+        message = '読解アヘンに更新があります\n'
+        for res in new:
+            message += '{title}: {link}\n'.format(**res)
         slack.chat.post_message('#general', message, as_user=True)
