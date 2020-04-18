9e1b26228ed49833e419ed6f1b24a138bd186973
kotapiku <ice08235523@gmail.com>
2020-04-18 22:31:03 +0900

    add

---
+++ cron/bathroulette.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import psycopg2
+import urllib.parse as urlparse
+import random
+
+from pyquery import PyQuery as pq
+from slacker import Slacker
+
+sys.path.append(os.path.abspath(os.curdir))
+
+from slackbot_settings import API_TOKEN
+
+
+if __name__ == '__main__':
+    slack = Slacker(API_TOKEN)
+    name1 = os.environ.get('NAME1', None)
+    name2 = os.environ.get('NAME2', None)
+
+    message = '今日のお風呂掃除は{}です'.format(random.choice([name1, name2]))
+    slack.chat.post_message('#times_mayuko', message, as_user=True)
